26dcafeb23d4265be7807456fda4efbe24408d67
AndyHou <andyhou965@gmail.com>
2024-05-05 18:59:03 +0400

    update the APIs for Wizard Page

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -1,18 +1,25 @@
 "use client";
 
+import React from "react";
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const RootProviders = ({ children }: { children: ReactNode }) => {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 export default RootProviders;
